d33e5eb2adc75928adf659e79d54d73550f982c6
agzet <agzetjik@gmail.com>
2025-12-20 23:20:07 +0600

    адаптация сделана но нужно еще потом доработать мини бро торопится

---
+++ java.js
@@ -0,0 +1,7 @@
+
+  const burger = document.getElementById('burger');
+  const nav = document.querySelector('.nav');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
